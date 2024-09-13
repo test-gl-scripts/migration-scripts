1f0081dde5188f155aaf430dda06e999819c7858
Avanthika Y <yavanthika@ceiamerica.com>
2024-09-13 15:20:35 +0530

    Update migrate-maven-packages-between-github-instances.sh

--- migrate-maven-packages-between-github-instances.sh
+++ migrate-maven-packages-between-github-instances.sh
@@ -40,6 +40,11 @@ auth_target="Authorization: token $GH_TARGET_PAT"
 # Fetch the list of Maven packages from the source organization
 packages=$(GH_HOST="$SOURCE_HOST" GH_TOKEN=$GH_SOURCE_PAT gh api --paginate "/orgs/$SOURCE_ORG/packages?package_type=maven" -q '.[] | .name + " " + .repository.name')
 echo "Packages response: $packages"
+# Check if any packages were fetched
+if [ -z "$packages" ]; then
+    echo "No packages found in $SOURCE_ORG"
+    exit 0
+fi
 
 # Iterate each package
 echo "$packages" | while IFS= read -r response; do
@@ -48,16 +53,22 @@ echo "$packages" | while IFS= read -r response; do
   package_name=$(echo "$response" | cut -d ' ' -f 1)
   repo_name=$(echo "$response" | cut -d ' ' -f 2)
   
+  # If repo_name is empty, skip
+    if [ -z "$repo_name" ]; then
+        echo "Repository name is empty for package $package_name, skipping..."
+        continue
+    fi
+    
   echo "org: $SOURCE_ORG repo: $repo_name --> package name $package_name"
 
   # Check if the repository exists in the target organization; if not, create it
-  # if ! GH_HOST="$TARGET_HOST" GH_TOKEN=$GH_TARGET_PAT gh repo view "$TARGET_ORG/$repo_name" >/dev/null 2>&1
-  # then
-  #   echo "Creating repo $TARGET_ORG/$repo_name"
-  #   GH_HOST="$TARGET_HOST" gh repo create "$TARGET_ORG/$repo_name" --private
-  # else
-  #   echo "Repo $TARGET_ORG/$repo_name already exists"
-  # fi
+  if ! GH_HOST="$TARGET_HOST" GH_TOKEN=$GH_TARGET_PAT gh repo view "$TARGET_ORG/$repo_name" >/dev/null 2>&1
+  then
+    echo "Creating repo $TARGET_ORG/$repo_name"
+    GH_HOST="$TARGET_HOST" gh repo create "$TARGET_ORG/$repo_name" --private
+  else
+    echo "Repo $TARGET_ORG/$repo_name already exists"
+  fi
 
   # Remove existing directory if it exists
   if [ -d "$repo_name" ]; then
@@ -66,49 +77,49 @@ echo "$packages" | while IFS= read -r response; do
   fi
 
   # Clone the repository from the source organization
- #  echo "Cloning repo from $SOURCE_ORG/$repo_name"  
- #  git clone "https://$GH_SOURCE_PAT@github.com/$SOURCE_ORG/$repo_name.git"
+  echo "Cloning repo from $SOURCE_ORG/$repo_name"  
+  git clone "https://$GH_SOURCE_PAT@github.com/$SOURCE_ORG/$repo_name.git"
 
- #  cd "$repo_name"
+  cd "$repo_name"
 
- #  # Update the remote URL to point to the target organization
- #  echo "Updating remote to point to target organization"
- #  git remote set-url origin "https://$GH_SOURCE_PAT@$TARGET_HOST/$TARGET_ORG/$repo_name.git"
+  # Update the remote URL to point to the target organization
+  echo "Updating remote to point to target organization"
+  git remote set-url origin "https://$GH_SOURCE_PAT@$TARGET_HOST/$TARGET_ORG/$repo_name.git"
 
- #  # Pull latest changes from the target repository if the main branch exists
- #  if git ls-remote --heads origin main | grep -q 'refs/heads/main'; then
- #      echo "Pulling latest changes from target repository"
- #      git pull origin main --rebase
- #  else
- #      echo "No main branch exists in the target repository. Skipping pull."
- #  fi
- # git config --global user.name rdesingraj
- # git config --global user.email rdesingraj@ceiamerica.com
- # echo "Git Setup done"
+  # Pull latest changes from the target repository if the main branch exists
+  if git ls-remote --heads origin main | grep -q 'refs/heads/main'; then
+      echo "Pulling latest changes from target repository"
+      git pull origin main --rebase
+  else
+      echo "No main branch exists in the target repository. Skipping pull."
+  fi
+ git config --global user.name rdesingraj
+ git config --global user.email rdesingraj@ceiamerica.com
+ echo "Git Setup done"
 
- #  # Update pom.xml if it exists
- #  if [ -f pom.xml ]; then
- #    echo "Updating pom.xml file to replace all instances of $SOURCE_ORG with $TARGET_ORG"
- #    sed -i 's|'"$SOURCE_ORG"'|'"$TARGET_ORG"'|g' pom.xml
- #    if git diff --quiet pom.xml; then
- #        echo "No changes in pom.xml, skipping commit."
- #    else    
- #        # Stage and Commit the pom.xml
- #        git add pom.xml
- #        git commit -m "Update pom.xml to point to TARGET_ORG"
- #        # Push changes to the main branch
- #        git push origin main
- #        git push origin --tags
- #    fi
- #  else
- #    echo "pom.xml file not found in the repo $repo_name"
- #  fi
+  # Update pom.xml if it exists
+  if [ -f pom.xml ]; then
+    echo "Updating pom.xml file to replace all instances of $SOURCE_ORG with $TARGET_ORG"
+    sed -i 's|'"$SOURCE_ORG"'|'"$TARGET_ORG"'|g' pom.xml
+    if git diff --quiet pom.xml; then
+        echo "No changes in pom.xml, skipping commit."
+    else    
+        # Stage and Commit the pom.xml
+        git add pom.xml
+        git commit -m "Update pom.xml to point to TARGET_ORG"
+        # Push changes to the main branch
+        git push origin main
+        git push origin --tags
+    fi
+  else
+    echo "pom.xml file not found in the repo $repo_name"
+  fi
 
- #  # Push to all branches
- #  git push origin --all
- #  cd ..
+  # Push to all branches
+  git push origin --all
+  cd ..
 
- #  echo "Repo $SOURCE_ORG/$repo_name cloned, pom.xml updated (if found), and pushed to $TARGET_ORG/$repo_name"
+  echo "Repo $SOURCE_ORG/$repo_name cloned, pom.xml updated (if found), and pushed to $TARGET_ORG/$repo_name"
 
   # Fetch Maven package versions from the source organization
   versions=$(GH_HOST="$SOURCE_HOST" GH_TOKEN=$GH_SOURCE_PAT gh api --paginate "orgs/$SOURCE_ORG/packages/maven/$package_name/versions" -q '.[] | .name' | sort -V)
