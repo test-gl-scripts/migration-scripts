ab7dab49fcd72746b58f28f577e84febdfa10fc9
Avanthika Y <yavanthika@ceiamerica.com>
2024-09-15 16:22:37 +0530

    Update migrate-maven-packages-between-github-instances.sh

--- migrate-maven-packages-between-github-instances.sh
+++ migrate-maven-packages-between-github-instances.sh
@@ -5,20 +5,31 @@ TARGET_ORG="computerenterprisesinc"
 repo_name="maven-sample"
 temp_dir="./temp"
 GH_TARGET_PAT="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"
+GH_SOURCE_PAT="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"
+
+# Create temp directories if they don't exist
+mkdir -p "${artifacts_dir}"
 
 # Example package details (replace with your actual logic for these values)
 package_group="com.cei"
 package_artifact="maven-package"
 versions=("0.0.1-SNAPSHOT" "0.1.1-SNAPSHOT" "5.1.1-SNAPSHOT")
 
+
+
 for version in "${versions[@]}"; do
     echo "   downloading: $package_group:$package_artifact:$version"
 
     # Download the Maven package from the source organization
-    curl -H "Authorization: token $GH_SOURCE_PAT" -L -o "${temp_dir}/artifacts/${package_artifact}-${version}.jar" \
+    curl -H "Authorization: token $GH_SOURCE_PAT" -L -o "${artifacts_dir}/${package_artifact}-${version}.jar" \
       "https://maven.pkg.github.com/$SOURCE_ORG/$repo_name/${package_group}/${package_artifact}/${version}/${package_artifact}-${version}.jar"
 
-    ls -lt
+    # Check if the file was created successfully
+    if [ ! -f "${artifacts_dir}/${package_artifact}-${version}.jar" ]; then
+        echo "Warning: Failed to create the file ${artifacts_dir}/${package_artifact}-${version}.jar"
+        continue
+    fi
+
     echo "   pushing: $package_group:$package_artifact:$version"
     
     upload_url="https://maven.pkg.github.com/$TARGET_ORG/$repo_name/${package_group}/${package_artifact}/${version}/${package_artifact}-${version}.jar"
@@ -27,13 +38,13 @@ for version in "${versions[@]}"; do
     # Upload the Maven package to the target organization
     response=$(curl -X PUT -H "Authorization: token $GH_TARGET_PAT" \
                   -H "Content-Type: application/java-archive" \
-                  --data-binary "@${temp_dir}/artifacts/${package_artifact}-${version}.jar" \
+                  --data-binary "@${artifacts_dir}/${package_artifact}-${version}.jar" \
                   "$upload_url" -w "%{http_code}" -o /dev/null -s)
     
     if [ "$response" -ne 200 ]; then
         echo "Upload failed with HTTP status code $response. Check the URL and repository setup."
         echo "URL: $upload_url"
-        echo "File: ${temp_dir}/artifacts/${package_artifact}-${version}.jar"
+        echo "File: ${artifacts_dir}/${package_artifact}-${version}.jar"
     else
         echo "Version got pushed...${package_artifact}-${version}"
     fi
