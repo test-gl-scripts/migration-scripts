15e40a95f09b6990e9e66e5452f5a80a7890f435
Avanthika Y <yavanthika@ceiamerica.com>
2024-09-15 13:43:34 +0530

    Update migrate-maven-packages-between-github-instances.sh

--- migrate-maven-packages-between-github-instances.sh
+++ migrate-maven-packages-between-github-instances.sh
@@ -87,16 +87,14 @@ echo "$packages" | while IFS= read -r response; do
     mvnfeed config stage_dir set --path $temp_dir/artifacts
 
     # Check if $TARGET_ORG/$repo_name exists in GitHub - if not, create it
-    # if ! GH_HOST="$TARGET_HOST" GH_TOKEN=$GH_TARGET_PAT gh api "/repos/$TARGET_ORG/$repo_name" >/dev/null 2>&1; then
-    #     echo "Creating repo $TARGET_ORG/$repo_name"
-    #     GH_HOST="$TARGET_HOST" GH_TOKEN=$GH_TARGET_PAT gh repo create "$TARGET_ORG/$repo_name" --private
-    # fi
+    if ! GH_HOST="$TARGET_HOST" GH_TOKEN=$GH_TARGET_PAT gh api "/repos/$TARGET_ORG/$repo_name" >/dev/null 2>&1; then
+        echo "Creating repo $TARGET_ORG/$repo_name"
+        GH_HOST="$TARGET_HOST" GH_TOKEN=$GH_TARGET_PAT gh repo create "$TARGET_ORG/$repo_name" --private
+    fi
 
     # Fetch package versions from the source registry
     versions=$(GH_HOST="$SOURCE_HOST" GH_TOKEN=$GH_SOURCE_PAT gh api --paginate "/orgs/$SOURCE_ORG/packages/maven/$package_name/versions" -q '.[] | .name' | sort -V)
     for version in $versions; do
-        # GitHub returns <groupId>.<artifactId>. Assuming no dots in artifactId,
-        # cut the components before and after the last dot
         package_group=$(echo "$package_name" | rev | cut -d '.' -f 2- | rev)
         package_artifact=$(echo "$package_name" | rev | cut -d '.' -f 1 | rev)
 
